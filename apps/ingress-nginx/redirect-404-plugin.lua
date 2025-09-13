f67ab15017f8f3f80bac4c9e83292ba12d12b5f3
mohi445257 <mohi445257@gmail.com>
2025-09-13 23:36:10 +0600

    Add Lua 404 redirect plugin for ingress-nginx

--- apps/ingress-nginx/redirect-404-plugin.lua
+++
@@ -1,9 +0,0 @@
-local _M = {}
-
-function _M.header_filter()
-  if ngx.status == ngx.HTTP_NOT_FOUND then
-    return ngx.redirect("https://www.google.com", ngx.HTTP_MOVED_TEMPORARILY)
-  end
-end
-
-return _M
